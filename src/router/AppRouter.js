a8e776a7dd648b9c15ed20597fb5c99c6d006472
Nelson Rosales <nelson.rosales83@gmail.com>
2024-04-18 10:14:25 +0200

    Update authentication and login/register pages

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -7,18 +7,25 @@ import {
 } from "react-router-dom"
 import ChatPage from "../pages/ChatPage"
 import AuthRouter from "./AuthRouter"
+import "../css/login-register.css"
 
 const AppRouter = () => {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/auth" component={AuthRouter} />
-          <Route exact path="/" component={ChatPage} />
-          <Redirect to="/" />
-        </Switch>
+    <div className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100 p-t-50 p-b-90">
+          <Router>
+            <div>
+              <Switch>
+                <Route path="/auth" component={AuthRouter} />
+                <Route exact path="/" component={ChatPage} />
+                <Redirect to="/" />
+              </Switch>
+            </div>
+          </Router>
+        </div>
       </div>
-    </Router>
+    </div>
   )
 }
 
