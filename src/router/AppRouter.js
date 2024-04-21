9170e6dd3758f8aed8f31b211897773b3caf1060
Nelson Rosales <nelson.rosales83@gmail.com>
2024-04-21 12:40:32 +0200

    Fixed authentication and login/register pages

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -7,25 +7,18 @@ import {
 } from "react-router-dom"
 import ChatPage from "../pages/ChatPage"
 import AuthRouter from "./AuthRouter"
-import "../css/login-register.css"
 
 const AppRouter = () => {
   return (
-    <div className="limiter">
-      <div className="container-login100">
-        <div className="wrap-login100 p-t-50 p-b-90">
-          <Router>
-            <div>
-              <Switch>
-                <Route path="/auth" component={AuthRouter} />
-                <Route exact path="/" component={ChatPage} />
-                <Redirect to="/" />
-              </Switch>
-            </div>
-          </Router>
-        </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/auth" component={AuthRouter} />
+          <Route exact path="/" component={ChatPage} />
+          <Redirect to="/" />
+        </Switch>
       </div>
-    </div>
+    </Router>
   )
 }
 
